66613aa5aed6f627bff893a8eaa5fa399a3d6af9
stryker <stryker@stryker-PC>
2015-05-02 00:52:25 -0300

    Aplicação do tema aristo do PrimeFaces. Configuração da criação da fábica de sessão do Hibernate. Criação da classe ContextListener - para inicialização e encerramento da fábrica de sessão.

---
+++ src/java/br/com/drogaria/util/ContextListener.java
@@ -0,0 +1,22 @@
+package br.com.drogaria.util;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ *
+ * @author stryker
+ */
+public class ContextListener implements ServletContextListener{
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        HibernateUtil.getSessionFactory().openSession();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+       HibernateUtil.getSessionFactory().close();
+    }
+    
+}
